0169f823d2b4eb47d6038cc3777c7145f91fe26e
Tanvik01 <115545334+Tanvik01@users.noreply.github.com>
2024-10-29 14:09:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,18 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Tanvi Kokitkar",
+  title: "Full Stack Developer",
+  email: "tanvikokitkar30@gmail.com",
+  gitHub: "https://github.com/Tanvik01",
+  instagram: "tanvi__01",
+  linkedIn: "www.linkedin.com/in/tanvi-kokitkar-500bb3242"
 };
 
-const primaryColor = "#4E567E";
+const primaryColor = "#87d49c";
 const secondaryColor = "#D2F1E4";
 
 const App = () => {
